9ce2707a8a70e4b93eca6b5a98b80f4bd5b5d9b4
Jefferson William Damasceno Silva <jwdamasceno24@outlook.com>
2024-10-30 20:55:12 -0300

    AULA 30/10/2024

---
+++ logica-de-programacao-javascript/ex0011/script.js
@@ -0,0 +1,5 @@
+var valorDolar = prompt("Digite o valor em Dólares:")
+var primeiroValor = parseFloat(primeiroValor)
+var metadeDoNumero = primeiroValor / 2
+
+alert(`O doblo do numero digitado é ${metadeDoNumero}`)
